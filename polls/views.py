1f20d47761acac1468571d94d3762aeb5e593f30
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-07-11 10:24:27 +0300

    valmis~

--- polls/views.py
+++ polls/views.py
@@ -1,28 +1,69 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-
+from django.views.decorators.csrf import csrf_exempt
 from .models import Question, Choice
-
-
+from django.utils import timezone
+from django.db import connection
+from datetime import datetime
+from dateutil.tz import tzutc
 """ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
  """
+@csrf_exempt
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
  
+@csrf_exempt
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+@csrf_exempt
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
+#2023-07-04 17:40:17.968230+00:00
+@csrf_exempt
+def create_new(request):
+    if request.method == "POST":
+        new_q = request.body.decode("utf-8").replace("create_new=","")
+        print(new_q)
+        count = 0
+        for p in Question.objects.all():
+            count += 1
+        newid = count+1
+        #q = Question(question_text=new_q, pub_date=timezone.now())
+        #q.save()
+        #q.id
+        #q.choice_set.create(choice_text='Not much', votes=0)
+        #q.choice_set.create(choice_text="the other answer", votes=0)
+        # sarakkeet ['id', 'question_text', 'pub_date']
+        aika = str(datetime.now(tz=tzutc()))
+        insert = f"INSERT INTO polls_question (id, question_text, pub_date) VALUES ( {str(newid)}, {str(new_q)}, {aika} )"
+        x = Question.objects.raw( insert)
+            #Question.objects.raw( )
+        # 'SELECT * FROM polls_question WHERE question_text="What's up?"'
+        pid = 0
+        #for p in Question.objects.raw('SELECT * FROM polls_question'):
+        #for p in Question.objects.raw('SELECT * FROM polls_question WHERE question_text="asd"'):
+        #    print(p.id)
+        #    i += 1
+        #    pid = p.id
+        haettu = Question.objects.get(pk=newid)
+        haettu.save()
+        #print("haettu:", haettu)
+        #haettu.delete()
+        #for p in Question.objects.all():
+        #    print(p)
+    return render(request, 'polls/create.html')
+
+@csrf_exempt
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
--- virheet.md
+++ virheet.md
@@ -1,8 +1,16 @@
-1. csrf
+Writer of this essay used OWASP 2012 top ten list. 
 
-2. logging
+1. csrf
+   @csrf_exempt remove those
+
+2. A09:2021-Security Logging and Monitoring Failures
+   doesn't exist
 
 3. A03:2021-Injection (tee joku)
 
-4. ssrf (race condition)
+4. ssrf (race condition) A10:2021 – Server-Side Request Forgery (SSRF)
+   exists for multiple users polling at the same exact moment
    https://docs.djangoproject.com/en/3.1/ref/models/expressions/#avoiding-race-conditions-using-f
+
+5. A01:2021 – Broken Access Control
+   vulnerability to path traversal as explained here https://security.snyk.io/vuln/SNYK-PYTHON-DJANGO-1298665 fixed by upgrading to higher version of Django.
